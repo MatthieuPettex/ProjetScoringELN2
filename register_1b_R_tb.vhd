f9f1c171b511fccc0a64752aee2dc24e07949b59
MatthieuPettex <72150356+MatthieuPettex@users.noreply.github.com>
2021-03-05 01:43:11 +0100

    Add files via upload

---
+++ register_1b_R_tb.vhd
@@ -0,0 +1,102 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   
+-- Design Name:   
+-- Module Name:   
+-- Project Name:  
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: 
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY register_1b_R_tb IS
+END register_1b_R_tb;
+
+ 
+ARCHITECTURE behavior OF register_1b_R_tb IS 
+ 
+    -- Component Declaration for the Unit Under Test (UUT)
+ 
+    COMPONENT register_1b_R
+    PORT(
+         R : IN  std_logic;
+         D : IN  std_logic;
+         clk : IN  std_logic;
+         Q : OUT  std_logic
+        );
+    END COMPONENT;
+    
+
+   --Inputs
+   signal R : std_logic := '0';
+   signal D : std_logic := '0';
+   signal clk : std_logic := '0';
+
+ 	--Outputs
+   signal Q : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: register_1b_R PORT MAP (
+          R => R,
+          D => D,
+          clk => clk,
+          Q => Q
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+--   -- Stimulus process
+--   stim_proc: process
+--   begin		
+--      -- hold reset state for 100 ns.
+--      wait for 100 ns;	
+--
+--      wait for clk_period*10;
+--
+--      -- insert stimulus here 
+--
+--      wait;
+--   end process;
+	
+
+-- [0,150ns] = cas normal, [150ns, 300ns] = cas pour le role de R
+D <= '0', '1' after 50 ns, '0' after 100 ns, '1' after 150 ns, '0' after 200 ns, '1' after 250 ns;
+R <= '0', '1' after 175 ns,'0' after 275 ns;
+
+END;
